22681eece2c578d736fcb10cf2b29962363abd2b
morvy666 <morvy0@gmail.com>
2020-02-18 19:27:03 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1582064060,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/selectedplayer-sheet0.png",
+		"images/selectedplayer-sheet1.png",
+		"images/portrait-sheet0.png",
+		"images/portrait-sheet1.png",
+		"images/portrait-sheet2.png",
+		"images/selectsquare-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
